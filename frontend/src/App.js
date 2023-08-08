bcad6a20362b64bc4eb272dacd011a8ce2cc65c6
Tharuka_00 <tharusenevi2000@gmail.com>
2023-08-09 02:03:08 +0530

    update navbar and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import {Routes, Route} from 'react-router-dom'
 
-
-const App = () => {
+function App() {
   return (
-    <div className="App">
+    <>
       <Navbar />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
   );
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,35 +1,44 @@
 import React, {useState} from "react";
-import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
+import {FaBars, FaTimes} from 'react-icons/fa'
 
 const Navbar = () => {
     const [nav, setNav] = useState(false);
-
-    const handleNav = () => {
-        setNav(!nav);
-    }
+    const handleClick = () => setNav(!nav);
 
     return (
-        <div className="bg-[#4b5563e1]">
-            <div className="flex justify-between items-center h-24 max-w-[1080px] mx-auto p-4">
-                <h1 className="w-full text-3xl font-bold text-[#00df9a]">Pathogen ID</h1>
-                <ul className="hidden md:flex uppercase">
-                    <li className="p-4 text-white">Home</li>
-                    <li className="p-4 text-white">About</li>
+        <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>                
+            
+            {/* Menu */}   
+            <h1 className="w-full text-3xl font-bold text-[#00df9a]">Pathogen ID</h1>
+            <ul className="hidden md:flex uppercase">
+                <li className="p-4 text-white">
+                    <a href="/">Home</a>
+                </li>
+                <li className="p-4 text-white">
+                    <a href="/about">About</a>
+                </li>
+            </ul>
+
+            {/* Hamburger */}
+            <div onClick={handleClick} className='md:hidden z-10'>
+                {!nav ? <FaBars /> : <FaTimes />}
+            </div>
+
+            {/* Mobile menu */}
+            <div>
+                <ul 
+                className={
+                    !nav
+                        ? 'hidden'
+                        : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+                }
+                >
+                    <li className="p-4 border-b border-[#414141] text-white"><a href="/">Home</a></li>
+                    <li className="p-4 border-b border-[#414141] text-white"><a href="/about">About</a></li>
                 </ul>
-
-                <div onClick={handleNav} className="block md:hidden">
-                    {!nav ? <AiOutlineClose size={20} /> :<AiOutlineMenu size={20} />}
-                </div>
-
-                <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-gray-600 ease-in-out duration-500' : 'fixed left-[-100%]'}>
-                    <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">Pathogen ID</h1>
-                    <ul className="uppercase p-4">
-                        <li className="p-4 border-b border-[#414141] text-white">Home</li>
-                        <li className="p-4 border-b border-[#414141] text-white">About</li>
-                    </ul>
-                </div>
             </div>
         </div>
+
     )
 }
 
